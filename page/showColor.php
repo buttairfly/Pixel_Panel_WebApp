64e87f7511d6d12dd7fe4b1cf1d4982c2642c62c
buttairfly <schkaty@web.de>
2016-07-31 22:29:48 +0000

    Fix showColor

--- page/showColor.php
+++ page/showColor.php
@@ -7,6 +7,45 @@
 		"spectrum" 	=> 0,
 	);
 	$bsCols = array('lg'=>12,'md'=>12,'sm'=>12,'xs'=>12);
-	
-	include "templates/body.php";
 ?>
+<script>
+	<?php if(!empty($Spectrums)) echo printVarSpectrum($Spectrums); ?>
+
+	function resize(){
+		<?php if(!empty($Spectrums)) echo printResizeSpectrum($Spectrums); ?>
+	}
+
+	function changeValue(){
+		<?php if(empty($_REQUEST['Site']))
+		{
+			echo $DefaultSite;
+		}
+		else 
+		{
+			echo $_REQUEST['Site'];//call function like the sites name
+		}?>(
+			<?php if(!empty($Spectrums)) echo printChangeValueSpectrum($Spectrums,$Komma["spectrum"]); ?>
+		);
+	}
+	
+	function paletteUpdateColor(){
+		changeValue();
+	}
+
+	$(document).ready(function() {
+		<?php if(!empty($Spectrums)) echo printInitSpectrum($Spectrums); ?>
+		
+		resize();
+		
+		$(window).resize(function(){
+			resize();
+		});
+	});
+ </script>
+<div class="row text-center">
+	<?php if(!empty($Spectrums)) {
+		echo printDivSpectrum($Spectrums,$bsCols);
+		echo printPaletteColors();
+	}
+	?>
+</div>
